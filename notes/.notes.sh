410c14b981aa116435ce4f61e9263c5c7afc55ce
Abdeljalil Letrache <abdeljalil@>
2021-01-30 16:20:42 +0100

    added different notes templates

--- notes/.notes.sh
+++ notes/.notes.sh
@@ -7,22 +7,34 @@ NC='\033[0m' # No Color
 
 notes_path="/home/abdeljalil/Documents/Notes"
 
-function notenew() {
+function nnew() {
 	printf "Choose notebook: "
 	new_note_notebook=$(find $notes_path -type d | fzf)
 	echo $new_note_notebook
 	printf 'Note title: '
 	read new_note_title
 	simplified_new_note_title=$new_note_title #to lowercase
+	
+	printf "Choose note type:\n1) Simple\n2) Math\n3) Code\nCreate note of type: "
+	read new_note_type_choice
+
+	if [[ "${new_note_type_choice}" == 1 ]]; then
+		new_note_type="texnote"
+	fi
+
+	if [[ "${new_note_type_choice}" == 2 ]]; then
+		new_note_type="texnotemath"
+	fi
+	
 #	simplified_new_note_title=${new_note_title,,} #to lowercase
 #	simplified_new_note_title="${simplified_new_note_title// /_}"
 #	simplified_new_note_title="${simplified_new_note_title/-/_}"
 #	simplified_new_note_title="${simplified_new_note_title//\'/}"
 #	touch "${new_note_notebook}/${simplified_new_note_title}.tex"
-	vim -c 'call feedkeys("itexnote\<Tab>")' -c "call feedkeys('${new_note_title}')" -c 'call feedkeys("\<Tab>")'  "${new_note_notebook}/${new_note_title}.tex" --servername tex 	
+vim -c 'call feedkeys("i")' -c "call feedkeys('${new_note_type}')" -c 'call feedkeys("\<Tab>")' -c "call feedkeys('${new_note_title}')" -c 'call feedkeys("\<Tab>")'  "${new_note_notebook}/${new_note_title}.tex" --servername tex 	
 }
 
-function notezip() {
+function nzip() {
 	for var in "$@"
 	do
 		if [[ "${var}" =~ .*\.tex ]]
@@ -34,7 +46,7 @@ function notezip() {
 #		echo "Note ${1} zipped"
 }
 
-function noteunzip() {
+function nunzip() {
 	for var in "$@"
 	do
 		IFS='.' read -ra notetounzip <<< "$var"
@@ -47,16 +59,16 @@ function notetree() {
 				tree -P "*.pdf" $notes_path
 }
 
-function notetitlesearch() {
+function ntsearch() {
 				tree -P "*${1}*" $notes_path
 }
 
-function noteopen() {
+function nopen() {
 	note_path=$(find $notes_path -regextype sed -regex ".*.pdf" | fzf)
 	zathura "${note_path}" & disown
 }
 
-function noteexists() {
+function nexists() {
 				if [ -f $1 ]
 				then
 								return 1
@@ -65,7 +77,7 @@ function noteexists() {
 				fi
 }
 
-function notesearch() {
+function nsearch() {
 	# Search recursively for a string, pass the output to grep and store each line in array element
 #	IFS=$'\n' files_list=($(pdfgrep "${1}" "${notes_path}" -r | grep -oP "^.${notes_path}([^:]*)"))
 
@@ -88,7 +100,7 @@ function notesearch() {
 	zathura "${BASH_REMATCH[1]}" -f "${1}" & disown
 }
 
-function notebooknew() {
+function nbnew() {
 	printf "Choose new notebook parent directory: "
 	new_notebook_directory=$(find $notes_path -type d | fzf)
 	echo $new_notebook_directory
@@ -98,7 +110,7 @@ function notebooknew() {
 	echo "Notebook ${new_notebook_name} created succesfully."
 }
 
-function notebookbrowse() {
+function nbbrowse() {
 	notebook_directory=$(find $notes_path -type d | fzf)
 	cd "${notebook_directory}"
 }
