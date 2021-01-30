ce664e99a35427f42a1cbc9ff3ab76aef99d9e0f
Abdeljalil Letrache <abdeljalil@>
2021-01-30 16:19:48 +0100

    added/modified shortcuts

--- scripts/.scripts/shortcuts.sh
+++ scripts/.scripts/shortcuts.sh
@@ -3,8 +3,15 @@
 shell_shortcuts=$HOME/.dotfiles/bash/.shortcuts.sh
 ranger_shortcuts=$HOME/.dotfiles/ranger/.config/ranger/shortcuts.conf
 
-awk '{ print "alias " $1 "=\"cd " $2"\""}' "shortcuts" > $shell_shortcuts
-awk '{ print "map g" $1 " cd " $2}' "shortcuts" > $ranger_shortcuts
+# Loop through n>=2 fields and store them in VAR
+# for fields n>2 append a space to the variable, so
+# path with spaces preserve the spaces.
+# For bash, replace the tilde (~) with $HOME since it
+# doesn't work in aliases
+
+awk '{VAR=""; for (i = 2; i<=NF; i++) {if(i!=2){VAR=VAR" "}; VAR=VAR$i};gsub("~","\$HOME",VAR); print "alias " $1 "=\"cd \047"VAR"\047\""}' "shortcuts"> $shell_shortcuts
+
+awk '{VAR=""; for (i = 2; i<=NF; i++) {if(i!=2){VAR=VAR" "}; VAR=VAR$i}; print "map g"$1 " cd "VAR}' "shortcuts" > $ranger_shortcuts
 
 cd ~/.dotfiles
 stow bash
