9b9cab863b87d122008b6a6072f3dce07df2f64d
Abdeljalil Letrache <abdeljalil@>
2021-02-21 21:36:35 +0100

    cleaned and restructured files

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -5,6 +5,6 @@ if [[ $state == 'Missing device address argument' ]]; then
 	echo 
 else
 	device_name=$(bluetoothctl info | grep Name | cut -d ' ' -f2)
-	echo  $device_name
+	echo  $device_name
 fi
 
